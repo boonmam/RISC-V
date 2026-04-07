b7d3da320a6f1ec3badb46785d5c4c8be4c09398
boonmam <boonmamccain@gmail.com>
2026-04-06 20:09:48 -0400

    Initial Project Setup in Vivado

---
+++ RISC-V/RISC-V.srcs/sources_1/new/RISC-V_top.sv
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/06/2026 08:07:37 PM
+// Design Name: 
+// Module Name: RISC-V_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RISC-V_top(
+
+
+
+    );
+endmodule
