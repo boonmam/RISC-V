81f2365cdd525257d4e787edfa0b071f9ce01559
boonmam <boonmamccain@gmail.com>
2026-04-06 20:48:22 -0400

    Added some I/O to top module

--- RISC-V/RISC-V.srcs/sources_1/new/RISC-V_top.sv
+++ RISC-V/RISC-V.srcs/sources_1/new/RISC-V_top.sv
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: McCain Boonma
+// Engineer: McCain Boonma
 // 
 // Create Date: 04/06/2026 08:07:37 PM
-// Design Name: 
+// Design Name: RISC-V FPGA Implementation
 // Module Name: RISC-V_top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
+// Project Name: RISC-V FPGA Implementation
+// Target Devices: Mimas A7 Mini FPGA Development Board
+// Tool Versions: Vivado 2025.2
+// Description: FPGA Implemnentation of a RISC-V processor. This module serves as the top-level design, integrating the processor core with input and output peripherals such as switches and LEDs.
 // 
 // Dependencies: 
 // 
@@ -21,8 +21,20 @@
 
 
 module RISC-V_top(
-
-
-
+    input clk,
+    input rst,
+    input [2:0] sw,     //Inputs from the switches to control the LEDs
+    output [7:0] LED,   // Output to the LEDs
+    output [2:0] RGB    // Output to the RGB LEDs
     );
+
+
+logic [7:0] LED_reg = 8'b0000_0001; // Register to hold the current state of the LEDs
+logic [2:0] RGB_reg = 3'b001; // Register to hold the current state of the RGB LEDs
+
+
+assign LED = LED_reg;
+assign RGB = RGB_reg;
+
+
 endmodule
