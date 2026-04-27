fa1b65d8f8be3cf17abf4addafbaa7b04886de12
boonmam <boonmamccain@gmail.com>
2026-04-27 00:12:59 -0400

    Further progress on state machine

--- RISC-V/RISC-V.srcs/sources_1/new/PMOD_SERIAL.sv
+++ RISC-V/RISC-V.srcs/sources_1/new/PMOD_SERIAL.sv
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 
 module PMOD_SERIAL #(
-    parameter int CLK_FREQ_HZ = 100_000_000
+    parameter int CLK_FREQ_HZ = 100_000_000,
     parameter int SPI_CLK_DIV = 32
 )(
     input  logic clk,
@@ -31,11 +31,9 @@ typedef enum logic [4:0] {
 oled_state_t oled_state_cur, oled_state_nxt;
 
 typedef enum logic [2:0] {
-    SPI_ST_RESET,
     SPI_ST_IDLE,
-    SPI_ST_START,
-    SPI_ST_LOAD,
-    SPI_ST_TRANSFER,
+    SPI_ST_SCLK_NEG,
+    SPI_ST_SCLK_POS,
     SPI_ST_STOP
   } spi_state_t;
 
@@ -44,7 +42,7 @@ spi_state_t spi_state_cur, spi_state_nxt;
 logic[31:0] clk_div_cnt;
 logic[7:0] spi_payload_1, spi_payload_2;
 logic[2:0] spi_bit_cnt;
-logic spi_busy, spi_done;
+logic spi_busy, spi_done, spi_bit_shift_strb;
 
 logic spi_start, spi_cmd_data, spi_init, spi_ack;
 
@@ -61,7 +59,7 @@ logic [31:0] delay_value;
 localparam int  WAIT20US = 20 * CLK_FREQ_HZ / 1_000_000;
 localparam int  WAIT50MS = 50 * CLK_FREQ_HZ / 1_000;
 localparam int  SCLK_PERIOD = SPI_CLK_DIV / CLK_FREQ_HZ * 1000000000; // 320ns 
-
+localparam int  SCLK_PERIOD_HALF = SPI_CLK_DIV / 2; // 16
 always_ff @(posedge clk ) begin
 
     if(rst) begin
@@ -113,7 +111,6 @@ always_comb begin
     VCCEN = 1'b0;
     PMODEN = 1'b0;
 
-    spi_reset = 1'b0;
     spi_init = 1'b0;
     spi_start = 1'b0;
     spi_cmd_data = 1'b0;
@@ -129,7 +126,6 @@ always_comb begin
             VCCEN = 1'b0;
             PMODEN = 1'b0;
 
-            spi_reset = 1'b1;
             spi_init = 1'b0;
             spi_start = 1'b0;
             spi_cmd_data = 1'b0;
@@ -197,16 +193,35 @@ end
 
 /// SPI State Machine /// 
 
+logic div_cnt_clr, div_cnt_inc;
+logic bit_cnt_clr, bit_cnt_inc;
+
+
 always_ff @(posedge clk ) begin
-    if(rst || spi_reset) begin
-        spi_state_cur <= SPI_ST_RESET;
+    if(rst) begin
+        spi_state_cur <= SPI_ST_IDLE;
+        clk_div_cnt <= 32'd0;
+        spi_bit_cnt <= 3'd0;
     end
 
     else begin
+
         spi_state_cur <= spi_state_nxt;
+
+        if(div_cnt_clr) begin
+            clk_div_cnt <= 32'd0;
+        end else if (div_cnt_inc) begin
+            clk_div_cnt <= clk_div_cnt + 1'd1;
+        end
+
+        if(bit_cnt_clr) begin
+            spi_bit_cnt <= 3'd0;
+        end else if (bit_cnt_inc) begin
+            spi_bit_cnt <= spi_bit_cnt + 1'd1;
+        end
+
     end
 end
-
 
 always_comb begin
 
@@ -220,8 +235,11 @@ always_comb begin
     spi_busy = 1'b0;
     spi_done = 1'b0;
 
-    spi_bit_cnt = 3'd0;
-    clk_div_cnt = 32'd0;
+    div_cnt_clr = 1'b0;
+    bit_cnt_clr = 1'b0;
+
+    div_cnt_inc = 1'b0;
+    bit_cnt_inc = 1'b0;
 
     case(spi_state_cur)
         SPI_ST_IDLE: begin
@@ -232,28 +250,80 @@ always_comb begin
             DCEN = 1'b0;
 
             spi_busy = 1'b0;
-            spi_busy = 1'b0;
-            spi_1_done = 1'b0;
-            spi_2_done = 1'b0;
+            spi_done = 1'b0;
 
-            spi_bit_cnt = 3'd0;
-            clk_div_cnt = 32'd0;
+            div_cnt_clr = 1'b1;
+            bit_cnt_clr = 1'b1;
 
-
+            div_cnt_inc = 1'b0;
+            bit_cnt_inc = 1'b0;
 
             if(spi_start) begin
-                spi_state_nxt = SPI_ST_RISING_EDGE;
+                spi_state_nxt = SPI_ST_SCLK_NEG;
+            end
+        end
+
+        SPI_ST_SCLK_NEG: begin
+            sclk = 1'b0;
+            mosi = spi_payload[7-spi_bit_cnt];
+            cs_n = 1'b0;
+            DCEN = spi_cmd_data;
+
+            spi_busy = 1'b1;
+
+            div_cnt_clr = 1'b0;
+            bit_cnt_clr = 1'b0;
+
+            div_cnt_inc = 1'b1;
+            bit_cnt_inc = 1'b0;
+
+
+
+            if (clk_div_cnt >= SCLK_PERIOD_HALF) begin
+                spi_state_nxt = SPI_ST_SCLK_POS;
             end
         end
 
         SPI_ST_SCLK_POS: begin
+            sclk = 1'b1;
+            mosi = spi_payload[7-spi_bit_cnt];
+            cs_n = 1'b0;
+            DCEN = spi_cmd_data;
+
+            spi_busy = 1'b1;
+
+            div_cnt_clr = 1'b0;
+            bit_cnt_clr = 1'b0;
+
+            div_cnt_inc = 1'b1;
+            bit_cnt_inc = 1'b0;
+
+
+            if (clk_div_cnt >= SCLK_PERIOD) begin
+                bit_cnt_inc = 1'b1;
+
+                if (spi_bit_cnt == 3'd7) begin
+                    spi_state_nxt = SPI_ST_STOP;
+                    spi_done = 1'b1;
+                end else begin
+                    spi_state_nxt = SPI_ST_SCLK_NEG;
+                end
+            end
         end 
 
-        SPI_ST__SCLK_NEG: begin
-        end
+        SPI_ST_STOP: begin
+            sclk = 1'b1;
+            mosi = 1'b0;
+            cs_n = 1'b1;
+            DCEN = 1'b0;
 
-        SPI_SI_STOP: begin
-            
+            spi_busy = 1'b0;
+            spi_done = 1'b1;
+
+            if (!spi_start) begin
+                spi_state_nxt = SPI_ST_IDLE;
+    
+            end
         end
     endcase
 end
